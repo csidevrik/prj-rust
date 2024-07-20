80035674e9eb268fd96f3062971774e175cbbd8b
linkfedok <carlos.sigua@gmail.com>
2024-07-20 12:22:31 -0500

    aprendiendo mas

---
+++ rext/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // Display the message "Hello, world!"
+    // todo!("Display the message by using the println!() macro");
+    // Este es un mensaje con la macro println!
+    println!("Este es un solo mensaje usando la macro println");
+    // Este es otro mensaje usando el primer y segundo argumento
+    println!("El primer caracter de la palabra Carlos es: {}",'C');
+    // Este es otro mensaje usando el primer, segundo argumento y tercer argumento
+    println!("The first letter of the English alphabet is {} and the last letter is {}.", 'A', 'Z');
+    // Veamos si soporta cuatro argumentos
+    println!("Tenemos tres argumentos el primero es {}, el segundo es {} y el tercero es {}.", 'A', 'Z', 'Y');
+}
