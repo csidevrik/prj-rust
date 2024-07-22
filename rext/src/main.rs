eeb9359ec080d71811be2418d90d3bff6eac63b0
carlos sigua <carlos.sigua@gmail.com>
2024-07-21 20:19:24 -0500

    hemos creado una libreria en rust en el mismo directorio

--- rext/src/main.rs
+++ rext/src/main.rs
@@ -2,7 +2,7 @@ use rext::{open_pdf_with_chrome, open_pdf_with_firefox};
 
 fn main() {
     let folder_path = "/home/adminos/OneDriveP/2A-JOB02-EMOVEP/2023/CONTRATOS/RE-EP-EMOVEP-2023-02/FACTURAS/CRUDO";
-    open_pdf_with_firefox(folder_path);
+    // open_pdf_with_firefox(folder_path);
     // o
-    // open_pdf_with_chrome(folder_path);
+    open_pdf_with_chrome(folder_path);
 }
